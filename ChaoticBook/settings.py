a2ed069ffdf689ff2c8261eefcb4d7fc6c208b32
Christos Barkonikos <xristosbarko@hotmail.com>
2021-06-02 23:21:58 +0300

    Adding heroku db settings back to the repo

--- ChaoticBook/settings.py
+++ ChaoticBook/settings.py
@@ -96,6 +96,13 @@ DATABASES = {
     }
 }
 
+# HEROKU SETTINGS
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+# END HEROKU SETTINGS
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
